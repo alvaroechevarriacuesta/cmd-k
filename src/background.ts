9a7e1daa354704ba2dd09b0c3dec99f27d8286e5
Alvaro Echevarria Cuesta <alvaroecuesta@gmail.com>
2025-09-10 21:56:57 -0400

    get rid of errors on refresh

--- src/background.ts
+++ src/background.ts
@@ -133,7 +133,7 @@ chrome.runtime.onMessage.addListener((message, _sender, sendResponse) => {
                     echo_refresh_token_expires_at:
                       response.tokenData.refreshTokenExpiresAt,
                   });
-                  sendResponse({ token: response.tokenData.accessToken });
+                  sendResponse({ success: true, token: response.tokenData.accessToken });
                 } else {
                   console.error("Error refreshing tokens:", response.error);
                   sendResponse({ token: null });
